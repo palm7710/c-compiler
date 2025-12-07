1da28ec0a1e1afb02ce58bd9dedd086cd2d2b33b
Palm <241it002i@s.internet.ac.jp>
2025-12-07 19:02:25 +0900

    足し算と引き算を追加

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -11,11 +11,35 @@ int main(int ac, char **av)
         return 1;
     }
 
+    char *p = av[1];
+    long val = strtol(p, &p, 10);
+
     // Intel記法
     printf("global _main\n");
     printf("section .text\n");
     printf("_main:\n");
     printf("    mov rax, %d\n", atoi(av[1]));
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-')
+        {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
+    }
+
     printf("    ret\n");
     return 0;
 }
