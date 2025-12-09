671c19ed1e44a089ea438aef42733ca421764b8a
Palm <241it002i@s.internet.ac.jp>
2025-12-10 00:27:22 +0900

    テストを変更

--- 9cc/test.sh
+++ 9cc/test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+GREEN='\033[32m'
+RED='\033[31m'
+CYAN='\033[36m'
+WHITE='\033[37m'
+RESET='\033[0m'
+
 cc -arch x86_64 9cc.c -o 9cc || exit 1
 
 assert() {
@@ -8,14 +14,14 @@ assert() {
 
     ./9cc "$input" > tmp.s
     nasm -f macho64 tmp.s -o tmp.o
-    cc -arch x86_64 tmp.o -o tmp
+    cc -arch x86_64 tmp.o -o tmp 2>/dev/null
     ./tmp
     actual="$?"
 
     if [ "$actual" = "$expected" ]; then
-        echo "$input => $actual"
+        echo -e "${CYAN}${input}${RESET} ${WHITE}=>${RESET} ${GREEN}${actual}${RESET}"
     else
-        echo "$input => $expected expected, but got $actual"
+        echo -e "${CYAN}${input}${RESET} ${WHITE}=>${RESET} ${RED}${expected} expected, but got ${actual}${RESET}"
         exit 1
     fi
 }
@@ -27,10 +33,10 @@ assert_error() {
     output=$(./9cc "$input" 2>&1 >/dev/null)
 
     if echo "$output" | grep -q "$expected"; then
-        echo "$input => error OK"
+        echo -e "${CYAN}${input}${RESET} ${WHITE}=>${RESET} ${GREEN}error OK${RESET}"
     else
-        echo "$input => expected error \"$expected\" but got:"
-        echo "$output"
+        echo -e "${CYAN}${input}${RESET} ${WHITE}=>${RESET} ${RED}expected error \"${expected}\" but got:${RESET}"
+        echo -e "${RED}${output}${RESET}"
         exit 1
     fi
 }
@@ -41,7 +47,10 @@ assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
 assert_error "1+3++" "数ではありません"
 assert_error "1 + foo + 5" "トークナイズできません"
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 
-echo OK
+echo -e "${GREEN}OK${RESET}"
 
 # Stop is `Ctrl + Z`
