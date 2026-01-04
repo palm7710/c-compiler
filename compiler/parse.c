d5f4fa3347117b9c6788329844324da109fb9a43
Palm <241it002i@s.internet.ac.jp>
2026-01-04 19:00:16 +0900

    sizeof を追加

--- compiler/parse.c
+++ compiler/parse.c
@@ -512,7 +512,7 @@ static Node *funcall(Token **rest, Token *tok) {
     return node;
 }
 
-// primary = "(" expr ")" | ident func-args? | num
+// primary = "(" expr ")" | "sizeof" unary | ident func-args? | num
 static Node *primary(Token **rest, Token *tok) {
     // 次のトークンが "(" なら、"(" expr ")"
     if (equal(tok, "(")) {
@@ -524,6 +524,12 @@ static Node *primary(Token **rest, Token *tok) {
         recursion_depth--;
         *rest = skip(tok, ")");
         return node;
+    }
+
+    if (equal(tok, "sizeof")) {
+        Node *node = unary(rest, tok->next);
+        add_type(node);
+        return new_num(node->ty->size, tok);
     }
 
     if (tok->kind == TK_IDENT) {
