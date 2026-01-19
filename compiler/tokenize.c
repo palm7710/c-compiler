5406eea30080d2ca23533aa252ce0275cd85fcb4
Palm <241it002i@s.internet.ac.jp>
2026-01-20 01:45:09 +0900

    \<octal-sequence>を追加する

--- compiler/tokenize.c
+++ compiler/tokenize.c
@@ -105,7 +105,21 @@ static bool is_keyword(Token *tok) {
     return false;
 }
 
-static int read_escaped_char(char *p) {
+static int read_escaped_char(char **new_pos, char *p) {
+    if ('0' <= *p && *p <= '7') {
+        // 8進数の数値を読み取る
+        int c = *p++ - '0';
+        if ('0' <= *p && *p <= '7') {
+            c = (c << 3) + (*p++ - '0');
+            if ('0' <= *p && *p <= '7')
+                c = (c << 3) + (*p++ - '0');
+        }
+        *new_pos = p;
+        return c;
+    }
+
+    *new_pos = p + 1;
+
     switch (*p) {
         case 'a': return '\a'; // ベル文字（端末でビープ音）
         case 'b': return '\b'; // バックスペース（1文字戻る）
@@ -139,8 +153,7 @@ static Token *read_string_literal(char *start) {
 
     for (char *p = start + 1; p < end;) {
         if (*p == '\\') {
-            buf[len++] = read_escaped_char(p + 1);
-            p += 2;
+            buf[len++] = read_escaped_char(&p, p + 1);
         } else {
             buf[len++] = *p++;
         }
