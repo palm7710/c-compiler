e4b0646fc2877f2b86f7ad53cf4e440d8daa7a9b
Palm <241it002i@s.internet.ac.jp>
2026-01-25 23:05:26 +0900

    行コメントとブロックコメントを追加する

--- compiler/test.sh
+++ compiler/test.sh
@@ -245,12 +245,16 @@ assert 10 'int main() { return - - +10; }'
 # assert 165 'int main() { return "\xA5"[0]; }'
 # assert 255 'int main() { return "\x00ff"[0]; }'
 
-echo -e "${CYAN}=== 今回のテスト ===${RESET}"
 # ステートメント
-assert 2 'int main() { return ({ 0; 1; 2; }); }'
-assert 1 'int main() { ({ 0; return 1; 2; }); return 3; }'
-assert 6 'int main() { return ({ 1; }) + ({ 2; }) + ({ 3; }); }'
-assert 3 'int main() { return ({ int x=3; x; }); }'
+# assert 2 'int main() { return ({ 0; 1; 2; }); }'
+# assert 1 'int main() { ({ 0; return 1; 2; }); return 3; }'
+# assert 6 'int main() { return ({ 1; }) + ({ 2; }) + ({ 3; }); }'
+# assert 3 'int main() { return ({ int x=3; x; }); }'
+
+echo -e "${CYAN}=== 今回のテスト ===${RESET}"
+assert 2 'int main() { /* return 1; */ return 2; }'
+assert 2 'int main() { // return 1;
+return 2; }'
 
 # セキュリティテスト
 echo -e "${CYAN}=== セキュリティテスト ===${RESET}"
